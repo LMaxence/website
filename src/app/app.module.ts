22c5030e4dec1ffe7fc12d4b35fc0fb5b3e1f3c8
Maxence Lecanu <maxencel@theodo.fr>
2020-09-10 10:04:53 +0200

    add responsive design for landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,25 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { IndexPageComponent } from "./pages/index/index-page.component";
 import { AboutPageComponent } from "./pages/about/about-page.component";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ButtonComponent } from './components/button/button.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ButtonComponent } from "./components/button/button.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-  declarations: [AppComponent, IndexPageComponent, AboutPageComponent, NavbarComponent, ButtonComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    IndexPageComponent,
+    AboutPageComponent,
+    NavbarComponent,
+    ButtonComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
